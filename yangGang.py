454f8e1ff027430eacd7964e03e804428bffd56d
NaveenBandarage <thebanterage@gmail.com>
2020-02-10 10:50:09 +1300

    tweets send links now

--- yangGang.py
+++ yangGang.py
@@ -15,11 +15,18 @@ auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
 #sending manual tweets
-status = input("Write your Yang related tweet here: ")
-api.update_status(status=status)
+# status = input("Write your Yang related tweet here: ")
+# api.update_status(status=status)
 
 #Testing tweeting at a user
 userName = api.user_timeline(screen_name="andrewyang")
 userTweet = userName[0]
 
-api.update_status('@andrewyang Humanity First', userTweet.id)
+# api.update_status('@andrewyang Humanity First', userTweet.id)
+
+#helpful for finding who follows me back or others.
+user = api.get_user('cnn')
+
+api.update_status('Hey, @cnn you need to learn some more about ubi! https://www.yang2020.com/policies/', [])
+# for friend in user.friends():
+#    print(friend.screen_name)
